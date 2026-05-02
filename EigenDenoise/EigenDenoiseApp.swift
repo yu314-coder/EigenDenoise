f0ad6072ac78cc124117a3ce11e42391e15e6665
yu314-coder <euler.yu@gmail.com>
2026-05-02 20:28:12 +0800

    Initial Commit

---
+++ EigenDenoise/EigenDenoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EigenDenoiseApp.swift
+//  EigenDenoise
+//
+//  Created by Euler on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct EigenDenoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
